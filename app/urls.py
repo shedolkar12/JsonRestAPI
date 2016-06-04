63289e1919ec347eedc2f2811e579c496bd04450
Rajesh Shedolkar <rajeshshedolkar12@gmail.com>
2016-06-04 15:34:23 +0530

    adding Provider API

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url, include
-
 from . import views
-
+# from .views import *
 #urls which send POST/GET data must not end with a slash
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    # url(r'^Provider/$', views.index, name='index'),
+    url(r'Provider/(?P<id>\w+)$', views.provider_info, name='provider_info'),
 ]
