37a91643fecaa145cbdf8b9fded87fbae3eb6450
Rajesh Shedolkar <rajeshshedolkar12@gmail.com>
2016-06-04 15:37:45 +0530

    adding Provider's API for GET,POST,PUT and DELETE

--- app/views.py
+++ app/views.py
@@ -9,4 +9,42 @@ import uuid,os
 from elasticsearch import Elasticsearch
 
 def index(request):
-    return HttpResponse("hello world")
+    # print(request.GET.dict())
+    # print(request.GET)
+    # Dict = request.GET.dict()
+    if request.method=='GET':
+        provider=Provider.objects.all()
+        s = ProviderSerializer(provider, many=True)
+        return Response(s.data)
+
+    elif request.method=='POST':
+        provider_id = uuid.uuid4().hex
+        data = request.data
+        if 'provider_id' not in data:
+            data['provider_id'] = provider_id
+
+        serial = ProviderSerializer(data=data)
+        if serial.is_valid():
+            serial.save()
+            return Response(serial.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serial.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def provider_info(request, id):
+
+    try:
+        prov = Provider.objects.filter(provider_id=id)
+    except Provider.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method=='GET':
+        s = ProviderSerializer(prov, many=True)
+        return Response(s.data)
+    elif request.method=='DELETE':
+        prov.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    # elif request.method=='PUT':
+
+    return HttpResponse('hi')
