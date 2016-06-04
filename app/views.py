d0c64a1e241fb2bab08a1c53048e54ed7aa8fa85
Rajesh Shedolkar <rajeshshedolkar12@gmail.com>
2016-06-04 15:36:54 +0530

    added some useful libraries

--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect, render_to_response
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse, Http404
 from app.models import *
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from app.serializers import ProviderSerializer
+import uuid,os
 from elasticsearch import Elasticsearch
 
 def index(request):
