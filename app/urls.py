9c139a36f36b0796d22d1f5bda30a3fb67dde896
Rajesh Shedolkar <rajeshshedolkar12@gmail.com>
2016-06-04 19:01:26 +0530

    added api for POST and GET  the provider's area

--- app/urls.py
+++ app/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     # url(r'^Provider/$', views.index, name='index'),
     url(r'Provider/(?P<id>\w+)$', views.provider_info, name='provider_info'),
+    url(r'Provider/(?P<id>\w+)/area/$', views.service_area, name='service_area'),
+    # url(r'Provider/(?P<provider_id>\w+)/area/(?P<area_id>\w+)$', views.service_area, name='service_area'),
 ]
