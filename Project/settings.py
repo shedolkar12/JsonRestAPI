8c20800104e5b44d189f8c39639f4bc3e44f0a32
Rajesh Shedolkar <rajeshshedolkar12@gmail.com>
2016-06-04 15:32:29 +0530

    added rest_framework app in the setting

--- Project/settings.py
+++ Project/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
+    'rest_framework',
 )
+
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
