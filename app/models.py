3e29a3fd29099bb18e31188d9f5a82ba4483e6b1
Rajesh Shedolkar <rajeshshedolkar12@gmail.com>
2016-06-04 15:33:50 +0530

    creating Provider Table

--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib import admin
+
+class Provider(models.Model):
+    provider_id=models.CharField(max_length=50)
+    name=models.CharField(max_length=50)
+    email=models.EmailField(max_length=50)
+    phone_number=models.CharField(max_length=50)
+    language=models.CharField(max_length=50)
+    currency=models.CharField(max_length=50)
+    def __str__(self):
+        return self.email
 
 # Create your models here.
